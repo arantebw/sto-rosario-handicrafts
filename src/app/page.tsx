759cd54b83f368fa35759bdb15dc06027b1491ed
Billy Arante <arantebillywilson@gmail.com>
2024-05-29 16:44:30 +0800

    feat(landing-page): ✨ align the components accordingly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,22 @@ export default function Home() {
       <div className="absolute top-4 right-4">
         <ModeToggle />
       </div>
-      <div className="w-1/2">
-        <h1>{process.env.APP_NAME}</h1>
-        <h2>{process.env.APP_DESCRIPTION}</h2>
+      <div className="w-1/2 flex flex-col justify-center">
+        <div className="w-3/4 mx-auto">
+          <h1 className="text-5xl text-center mb-4">{process.env.APP_NAME}</h1>
+          <h2 className="text-center">{process.env.APP_DESCRIPTION}</h2>
+        </div>
       </div>
-      <div className="w-1/2">
-        <ButtonLink label="Access an existing account" path="/login" />
-        <ButtonLink label="Create a new account" path="/register" />
+      <div className="w-1/2 flex flex-col justify-center">
+        <div className="w-1/2 mx-auto flex flex-col justify-center gap-4">
+          <ButtonLink label="Access an existing account" path="/login" />
+          <ButtonLink label="Create a new account" path="/register" />
+        </div>
       </div>
     </main>
   );
 }
+
+
 
 
