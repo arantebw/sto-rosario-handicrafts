45c65e1b60430112fe59364baf63a7717e6f3118
Billy Arante <arantebillywilson@gmail.com>
2024-06-14 22:34:07 +0800

    feat: ✨ add link to /products page from the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import ButtonLink from "@/components/button-link";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Separator } from "@/components/ui/separator";
 import WelcomeSection from "@/components/welcome-section";
+import Link from "next/link";
 
 function HomePage() {
   return (
@@ -11,6 +13,10 @@ function HomePage() {
       <WelcomeSection />
       <div className="w-1/2 flex flex-col justify-center">
         <div className="w-1/2 mx-auto flex flex-col justify-center gap-4">
+          <p>
+            Let's go and <Link className="text-green-600 hover:underline" href="/products">start shopping</Link>!
+          </p>
+          <Separator />
           <ButtonLink label="Login" path="/login" />
           <ButtonLink label="Register" path="/register" />
         </div>
