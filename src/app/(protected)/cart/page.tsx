caa7b2724e5ad36de5f7d6b4e0b4a513523f15e0
Billy Arante <arantebillywilson@gmail.com>
2024-06-20 16:39:03 +0800

    feat(cart): ✨ implement the initial layout design

---
+++ src/app/(protected)/cart/page.tsx
@@ -0,0 +1,26 @@
+import NavFooter from "@/components/nav-footer";
+import NavHeader from "@/components/nav-header";
+
+function CartPage() {
+  return (
+    <main className="max-w-[1440px] mx-auto flex flex-col h-screen relative">
+      <NavHeader />
+      <main className="p-4 flex flex-col gap-4">
+        <div>
+          <h2 className="text-2xl">Shopping Cart</h2>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-4">
+          <div className="w-full lg:w-2/3 p-4 rounded-md border">
+            <h3 className="text-xl"></h3>
+          </div>
+          <div className="w-full lg:w-1/3 p-4 rounded-md border">
+            <h3 className="text-xl">Cart Summary</h3>
+          </div>
+        </div>
+      </main>
+      <NavFooter />
+    </main>
+  );
+}
+
+export default CartPage
