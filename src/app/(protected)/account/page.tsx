404e0d7e718baf2df4165cda0be6b02b89277377
Billy Arante <arantebillywilson@gmail.com>
2024-08-29 15:37:15 +0800

    feat((protected)): add auth to protected routes

--- src/app/(protected)/account/page.tsx
+++ src/app/(protected)/account/page.tsx
@@ -1,30 +1,35 @@
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+import { PersonalDetails } from "@/components";
 import NavFooter from "@/components/nav-footer";
 import NavHeader from "@/components/nav-header";
-import { PersonalDetails } from "@/components";
 
-export default async function AccountPage() {
-  return (
-    <main className="max-w-[1440px] mx-auto flex flex-col h-screen relative">
-      <NavHeader />
-      <main className="p-4 grid grid-cols-1 gap-4">
-        <div>
-          <h2 className="text-2xl">My Account</h2>
-        </div>
-        <div className="flex flex-col md:flex-row gap-4">
-          <div className="w-full md:w-1/5 gap-4">
-            <ul className="flex flex-col gap-4">
-              <li>Personal Details</li>
-              <li>Address Book</li>
-              <li>My Orders</li>
-              <li>
-                <a href="/api/auth/logout">Log Out</a>
-              </li>
-            </ul>
+export default withPageAuthRequired(
+  async function AccountPage() {
+    return (
+      <main className="max-w-[1440px] mx-auto flex flex-col h-screen relative">
+        <NavHeader />
+        <main className="p-4 grid grid-cols-1 gap-4">
+          <div>
+            <h2 className="text-2xl">My Account</h2>
           </div>
-          <PersonalDetails />
-        </div>
+          <div className="flex flex-col md:flex-row gap-4">
+            <div className="w-full md:w-1/5 gap-4">
+              <ul className="flex flex-col gap-4">
+                <li>Personal Details</li>
+                <li>Address Book</li>
+                <li>My Orders</li>
+                <li>
+                  <a href="/api/auth/logout">Log Out</a>
+                </li>
+              </ul>
+            </div>
+            <PersonalDetails />
+          </div>
+        </main>
+        <NavFooter />
       </main>
-      <NavFooter />
-    </main>
-  );
-}
+    );
+  },
+  { returnTo: "/account" },
+);
--- src/app/(protected)/wishlist/page.tsx
+++ src/app/(protected)/wishlist/page.tsx
@@ -1,22 +1,18 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 
-import { CartSummary, ShoppingCart } from "@/components";
 import NavFooter from "@/components/nav-footer";
 import NavHeader from "@/components/nav-header";
 
 export default withPageAuthRequired(
-  async function CartPage() {
+  async function WishlistPage() {
     return (
       <main className="max-w-[1440px] mx-auto flex flex-col h-screen relative">
         <NavHeader />
         <main className="flex flex-col gap-4 p-4">
           <div>
-            <h2 className="text-2xl">Shopping Cart</h2>
+            <h2 className="text-2xl">Wishlist</h2>
           </div>
-          <div className="flex flex-col lg:flex-row gap-4">
-            <ShoppingCart />
-            <CartSummary />
-          </div>
+          <div className="flex flex-col lg:flex-row gap-4"></div>
         </main>
         <NavFooter />
       </main>
