2d02ebfe06461659a011ad7c8e660cf587facd35
Billy Arante <arantebillywilson@gmail.com>
2024-10-31 09:31:08 +0800

    feat(sales): ✨ add the route and page

---
+++ src/components/sales-details/index.tsx
@@ -0,0 +1,8 @@
+export default function SalesDetails() {
+  return (
+    <div className="flex-grow p-4 rounded-md border">
+      <h3 className="text-xl">Sales</h3>
+      <hr className="my-4" />
+    </div>
+  );
+}
