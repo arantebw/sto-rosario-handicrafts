c6253c8f4481b9439e648e4218dcd398ca0e586b
Billy Arante <arantebillywilson@gmail.com>
2024-10-31 10:33:57 +0800

    feat: ✨ integrate radix-ui themes

--- src/components/inventory-details/index.tsx
+++ src/components/inventory-details/index.tsx
@@ -1,8 +1,41 @@
+"use client";
+
+import { Table } from "@radix-ui/themes";
+
 export default function InventoryDetails() {
   return (
     <div className="flex-grow p-4 rounded-md border">
       <h3 className="text-xl">Inventory</h3>
       <hr className="my-4" />
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Full name</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Email</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Group</Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.RowHeaderCell>Danilo Sousa</Table.RowHeaderCell>
+            <Table.Cell>danilo@example.com</Table.Cell>
+            <Table.Cell>Developer</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Zahra Ambessa</Table.RowHeaderCell>
+            <Table.Cell>zahra@example.com</Table.Cell>
+            <Table.Cell>Admin</Table.Cell>
+          </Table.Row>
+
+          <Table.Row>
+            <Table.RowHeaderCell>Jasper Eriksson</Table.RowHeaderCell>
+            <Table.Cell>jasper@example.com</Table.Cell>
+            <Table.Cell>Developer</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table.Root>
     </div>
   );
 }
