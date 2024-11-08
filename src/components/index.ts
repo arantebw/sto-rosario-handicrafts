09a557fea8f2d2b9d9db44721fd58410ab5ddbe1
Billy Arante <arantebillywilson@gmail.com>
2024-11-08 23:01:21 +0800

    refactor(components): ♻️ export the ProductInventoryDetails component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,17 @@
 import { default as AddressForm } from "./address-form";
 import { default as CartSummary } from "./cart-summary";
 import { default as CheckoutProcess } from "./checkout-process";
-import { default as PersonalDetails } from "./personal-details";
-import { default as ShoppingCart } from "./shopping-cart";
 import { default as ComboBox } from "./province-combobox";
+import { default as PersonalDetails } from "./personal-details";
+import { default as ProductInventoryDetails } from "./product-inventory-details";
+import { default as ShoppingCart } from "./shopping-cart";
 
 export {
   AddressForm,
   CartSummary,
   CheckoutProcess,
-  PersonalDetails,
   ComboBox,
+  PersonalDetails,
+  ProductInventoryDetails,
   ShoppingCart,
 };
