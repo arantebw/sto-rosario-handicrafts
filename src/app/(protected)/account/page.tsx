9cbfcd407960d2aaa905c7d1617d154721ff7770
Billy Arante <arantebillywilson@gmail.com>
2024-11-08 21:32:35 +0800

    refactor(account): ♻️ use the layout in the main account page

--- src/app/(protected)/account/page.tsx
+++ src/app/(protected)/account/page.tsx
@@ -1,43 +1,9 @@
-import { withPageAuthRequired, getSession } from "@auth0/nextjs-auth0";
-
-import { createUser, retrieveOneUserByEmail } from "@/actions";
-import NavFooter from "@/components/nav-footer";
-import NavHeader from "@/components/nav-header";
-import { User } from "@/types";
-import AccountSidebar from "@/components/account-sidebar";
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { PersonalDetails } from "@/components";
 
 export default withPageAuthRequired(
   async function AccountPage() {
-    const session = await getSession();
-
-    let user: User = await retrieveOneUserByEmail(session?.user.email);
-    if (!user) {
-      user = await createUser({
-        email: session?.user.email,
-        name: {
-          firstName: session?.user.given_name,
-          lastName: session?.user.family_name,
-        },
-      });
-    }
-
-    return (
-      <main className="max-w-[1440px] mx-auto flex flex-col h-screen relative">
-        <NavHeader />
-        <main className="p-4 grid grid-cols-1 gap-4">
-          <div>
-            <h2 className="text-2xl">{`Hello, ${user.name.firstName}!`}</h2>
-          </div>
-          <div className="flex flex-col md:flex-row gap-4">
-            <AccountSidebar>
-              <PersonalDetails />
-            </AccountSidebar>
-          </div>
-        </main>
-        <NavFooter />
-      </main>
-    );
+    return <PersonalDetails />;
   },
   { returnTo: "/account" },
 );
