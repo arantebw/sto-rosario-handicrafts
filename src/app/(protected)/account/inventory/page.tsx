3ea3eac9d4aa9ece61a5e2447633ba42633bd22e
Billy Arante <arantebillywilson@gmail.com>
2024-11-30 23:05:15 +0800

    feat: ✨ redirect user to /accounts if not admin type

--- src/app/(protected)/account/inventory/page.tsx
+++ src/app/(protected)/account/inventory/page.tsx
@@ -1,8 +1,21 @@
+import { retrieveOneUserByEmail } from "@/actions";
 import InventoryDetails from "@/components/inventory-details";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { User } from "@/types";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
 
 export default withPageAuthRequired(
   async function Inventory() {
+    const session = await getSession();
+    if (!session) {
+      redirect("/api/auth/login");
+    }
+    let user: User = await retrieveOneUserByEmail(session?.user.email);
+
+    if (user?.role === "regular") {
+      redirect("/account");
+    }
+
     return <InventoryDetails />;
   },
   { returnTo: "/account/inventory" },
--- src/app/(protected)/account/sales/page.tsx
+++ src/app/(protected)/account/sales/page.tsx
@@ -1,8 +1,21 @@
+import { retrieveOneUserByEmail } from "@/actions";
 import SalesDetails from "@/components/sales-details";
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { User } from "@/types";
+import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { redirect } from "next/navigation";
 
 export default withPageAuthRequired(
   async function Sales() {
+    const session = await getSession();
+    if (!session) {
+      redirect("/api/auth/login");
+    }
+    let user: User = await retrieveOneUserByEmail(session?.user.email);
+
+    if (user?.role === "regular") {
+      redirect("/account");
+    }
+
     return <SalesDetails />;
   },
   { returnTo: "/account/sales" },
