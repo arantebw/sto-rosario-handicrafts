cb66d3464a360c9ec2b23f8bf6bbb63691d47981
Billy Arante <arantebillywilson@gmail.com>
2024-12-01 14:41:11 +0800

    feat: ✨ add the address listing component

---
+++ src/components/address-listing/index.tsx
@@ -0,0 +1,8 @@
+export default function AddressListing() {
+  return (
+    <div className="flex-grow p-4 rounded-md border">
+      <h3 className="text-xl">List of Address</h3>
+      <hr className="my-4" />
+    </div>
+  );
+}
