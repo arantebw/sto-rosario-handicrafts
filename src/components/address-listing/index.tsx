4581a2c251a44e8f4e112c39dd692ee36a3f0a78
Billy Arante <arantebillywilson@gmail.com>
2024-12-01 17:34:42 +0800

    feat: ✨ add the address-listing component

--- src/components/address-listing/index.tsx
+++ src/components/address-listing/index.tsx
@@ -1,7 +1,12 @@
+import NewAddressModal from "../new-address-modal";
+
 export default function AddressListing() {
   return (
     <div className="flex-grow p-4 rounded-md border">
-      <h3 className="text-xl">List of Address</h3>
+      <div className="flex justify-between items-center">
+        <h3 className="text-xl">List of Address</h3>
+        <NewAddressModal />
+      </div>
       <hr className="my-4" />
     </div>
   );
